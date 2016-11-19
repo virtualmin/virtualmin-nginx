af1f38912b11ffd06a185ca43d898e897f6aba74
Jamie Cameron <jcameron@webmin.com>
2016-11-19 14:53:15 -0800

    The 206 code indicates a partial download which should be counted https://github.com/webmin/webmin/issues/459

--- virtual_feature.pl
+++ virtual_feature.pl
@@ -1231,7 +1231,7 @@ foreach my $l (&unique(@logs)) {
 			open($LOG, "<", $f);
 			}
 		while(<$LOG>) {
-			if (/^(\S+)\s+(\S+)\s+(\S+)\s+\[(\d+)\/(\S+)\/(\d+):(\d+):(\d+):(\d+)\s+(\S+)\]\s+"([^"]*)"\s+(\S+)\s+(\S+)/ && $12 ne "206") {
+			if (/^(\S+)\s+(\S+)\s+(\S+)\s+\[(\d+)\/(\S+)\/(\d+):(\d+):(\d+):(\d+)\s+(\S+)\]\s+"([^"]*)"\s+(\S+)\s+(\S+)/) {
 				# Valid-looking log line .. work out the time
 				no warnings "once";
 				my $ltime = timelocal($9, $8, $7, $4, $virtual_server::apache_mmap{lc($5)}, $6-1900);
